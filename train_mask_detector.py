b2b2ea2f1acb0797b2f76a487edcd212ffc581cf
Ananthalekshmy_007 <47980194+Ananthalekshmy007@users.noreply.github.com>
2020-07-31 13:40:04 +0530

    Update train_mask_detector.py

--- train_mask_detector.py
+++ train_mask_detector.py
@@ -120,7 +120,7 @@ print(classification_report(testY.argmax(axis=1), predIdxs,
 
 # serialize the model to disk
 print("[INFO] saving mask detector model...")
-model.save("model", save_format="h5")
+model.save("model.h5", save_format="h5")
 
 # plot the training loss and accuracy
 N = EPOCHS
